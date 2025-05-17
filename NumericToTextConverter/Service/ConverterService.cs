b0908106f5c0360848208af4a9221d5e20e68514
lord-snoot <pip.j.bruyeres@gmail.com>
2025-05-17 22:25:08 +1000

    Add ConverterService initial

---
+++ NumericToTextConverter/Service/ConverterService.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Encodings.Web;
+
+namespace NumericToTextConverter.Service;
+
+public class ConverterService
+{
+    public string Convert(string input)
+    {
+        
+        return input;
+    }
+}
