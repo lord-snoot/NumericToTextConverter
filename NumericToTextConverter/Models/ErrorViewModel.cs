4c2f9897c17c39552660ff89ac5d3b3e0cb916c4
lord-snoot <pip.j.bruyeres@gmail.com>
2025-05-17 15:05:41 +1000

    Initial commit

---
+++ NumericToTextConverter/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NumericToTextConverter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
