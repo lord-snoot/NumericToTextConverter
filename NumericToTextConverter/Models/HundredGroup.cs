ff1cd1e978a0ef0951ea38048915010c1c1061a9
Pip Bruyeres <pip.j.bruyeres@gmail.com>
2025-05-18 12:22:10 +1000

    Initial models for HundredGroup to represent three numbers and NumericText to represent the whole number as text

---
+++ NumericToTextConverter/Models/HundredGroup.cs
@@ -0,0 +1,25 @@
+﻿namespace NumericToTextConverter.Models;
+
+public class HundredGroup
+{
+    public HundredGroup(string number)
+    {
+        List<string> numberList = number.Split("").ToList();
+        numberList.Reverse();
+        Ones = int.Parse(numberList[0]);
+        
+        if (numberList.Count > 1)
+        {
+            Tens = int.Parse(numberList[1]);
+        }
+
+        if (numberList.Count > 2)
+        {
+            Hundreds = int.Parse(numberList[2]);
+        }
+    }
+
+    private int Ones {get; set;}
+    private int Tens {get; set;}
+    private int Hundreds {get; set;}
+}
---
+++ NumericToTextConverter/Models/NumericText.cs
@@ -0,0 +1,11 @@
+﻿namespace NumericToTextConverter.Models;
+
+public class NumericText
+{
+    private Dictionary<int, HundredGroup> HundredGroups = new Dictionary<int, HundredGroup>();
+
+    public void AddHundredGroup(int pos, HundredGroup hundredGroup)
+    {
+        HundredGroups.Add(pos, hundredGroup);
+    }
+}
